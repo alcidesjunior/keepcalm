619b664bbf5049451f5f18e479bc08437d7aeacc
Alcides Junior <alcidesjunior.infor@gmail.com>
2020-04-07 00:24:06 -0300

    exercises ok

--- Keep Calm/Controller/TabViewController.swift
+++ Keep Calm/Controller/TabViewController.swift
@@ -31,5 +31,6 @@ class TabViewController: UITabBarController {
 
         let controllers = [homeViewController, routinesViewController, exercisesViewController, foodViewController, anxietyViewController]
         self.viewControllers = controllers
+        self.selectedIndex = 2
     }
 }
