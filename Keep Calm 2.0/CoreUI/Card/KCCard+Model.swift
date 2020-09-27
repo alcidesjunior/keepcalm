10da6e88aade5847942956cc5e78832fe772ead3
alcidesjunior <alcidesjunior.infor@gmail.com>
2020-09-27 14:15:26 -0300

    remove old folder

--- Keep Calm 2.0/CoreUI/Card/KCCard+Model.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-extension KCCard {
-    struct Model {
-        let text: KCLabel
-        let image: String?
-        let background: Color
-    }
-}
--- Keep Calm 2.0/CoreUI/TextArea/KCTextView+Model.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-//extension KCTextView {
-//    struct Model {
-//        @Binding var text: String
-//        let width: CGFloat?
-//        let height: CGFloat
-//    }
-//}
